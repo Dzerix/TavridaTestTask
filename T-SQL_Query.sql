d77382e29d387cece120b4aa2bd46675f06ad966
Дмитрий Балабин <dmitriy.balabin@radarit.ru>
2023-04-09 17:06:20 +0300

    Оптимизировал sql запрос и изменил его так, чтобы он мог быть выполнен на MSSQL Server 2012

--- T-SQL_Query.sql
+++ T-SQL_Query.sql
@@ -1,12 +1,16 @@
-select
-cb.Name as CompanyBranchName,
-c.Name as CompanyBranchName,
-c.BinarySign as CompanyGroup,
-case
-when c.BinarySign = 0
-	then (select STRING_AGG(cb.Name, ', ') from dbo.CompaniesBranches cb where cb.CompanyId = c.Id)
-when c.BinarySign = 1
-	then (select STRING_AGG(cb.Name, ', ') from dbo.CompaniesBranches cb)
-end as RelatedBranches
-from dbo."CompaniesBranches" cb
-join dbo."Companies" c on cb."CompanyId" = c."Id";
+SELECT 
+    cb.Name AS CompanyBranchName,
+    c.Name AS CompanyName,
+    c.BinarySign AS CompanyGroup,
+    CASE
+        WHEN c.BinarySign = 0
+            THEN STUFF((SELECT ', ' + cb1.Name 
+                        FROM dbo.CompaniesBranches cb1 
+                        WHERE cb1.CompanyId = c.Id 
+                        FOR XML PATH ('')), 1,2, '')
+        ELSE STUFF((SELECT ', ' + cb2.Name 
+                    FROM dbo.CompaniesBranches cb2 
+                    FOR XML PATH ('')), 1, 2, '')
+    END AS RelatedBranches
+FROM dbo.CompaniesBranches cb
+JOIN dbo.Companies c ON cb.CompanyId = c.Id;
